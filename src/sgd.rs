087fc05863326eb12a20429e235514cff25e0f06
Peter Jin <peterhaijin@gmail.com>
2017-03-07 15:42:57 -0800

    Use the `persist` method instead of `rollover`. Implement a constructor that uses the `val_size` method to determine the dimensionality of the parameter variables.

--- src/sgd.rs
+++ src/sgd.rs
@@ -1,7 +1,8 @@
 use arraydiff::prelude::*;
 use densearray::prelude::*;
 
-use rand::chacha::{ChaChaRng};
+//use rand::chacha::{ChaChaRng};
+//use std::cell::{RefCell};
 use std::cmp::{min};
 use std::rc::{Rc};
 
@@ -11,9 +12,32 @@ pub trait Schedule {
 
 pub struct ConstantSchedule(pub f64);
 
-pub struct PiecewiseSchedule {
-  pub init_value:   f64,
-  pub pieces:       Vec<(usize, f64)>,
+impl Schedule for ConstantSchedule {
+  fn at_iter(&self, _iter_nr: usize) -> f64 {
+    self.0
+  }
+}
+
+pub struct PiecewiseStepSchedule {
+  pub init:     f64,
+  pub pieces:   Vec<(usize, f64)>,
+}
+
+impl Schedule for PiecewiseStepSchedule {
+  fn at_iter(&self, iter_nr: usize) -> f64 {
+    if self.pieces.is_empty() {
+      return self.init;
+    }
+    for k in 0 .. self.pieces.len() {
+      if iter_nr < self.pieces[k].0 {
+        match k {
+          0 => return self.init,
+          _ => return self.pieces[k-1].1,
+        }
+      }
+    }
+    self.pieces[self.pieces.len()-1].1
+  }
 }
 
 pub struct SgdConfig {
@@ -24,13 +48,13 @@ pub struct SgdConfig {
 }
 
 pub struct Sgd {
-  cfg:      SgdConfig,
+  cfg:          SgdConfig,
   consts_params:        VarSet,
   consts_params_grads:  VarSet,
-  params:               VarSet,
-  grads:                VarSet,
-  obj:      Rc<AutodiffSink>,
-  iter_nr:  usize,
+  params:       VarSet,
+  grads:        VarSet,
+  obj:          Rc<AutodiffSink>,
+  iter_nr:      usize,
   param:        Vec<f32>,
   grad:         Vec<f32>,
   prev_param:   Array1d<f32>,
@@ -38,26 +62,43 @@ pub struct Sgd {
 }
 
 impl Sgd {
-  pub fn new(obj: Rc<AutodiffSink>, const_vars: VarSet, param_vars: VarSet) -> Self {
-    let new_txn = txn();
-    let consts = const_vars.filter(|v| v.kind == Val);
-    let params = param_vars.filter(|v| v.kind == Val);
+  pub fn new(cfg: SgdConfig, obj: Rc<AutodiffSink>, param_vars: VarSet, const_vars: VarSet) -> Self {
+    let mut params = param_vars.filter(|v| v.kind == Val);
     let grads = param_vars.filter(|v| v.kind == Grad);
-    let consts_params = consts.clone().union(params.clone());
+    let consts = const_vars.filter(|v| v.kind == Val);
+    let mut consts_params = consts.clone().union(params.clone());
     let consts_params_grads = consts.union(params.clone()).union(grads.clone());
-    /*let param_dim = obj.serial_size(new_txn, &mut params);
-    let grad_dim = obj.serial_size(new_txn, &mut grads);
-    assert_eq!(param_dim, grad_dim);*/
-    unimplemented!();
-  }
 
-  pub fn reset(&mut self, rng: &mut ChaChaRng) {
-    let init_txn = txn();
-    // FIXME
-    //self.obj.init(init_txn, rng);
-    self.obj.store_val(init_txn, &mut self.params, 0, &mut self.param);
-    self.prev_param.as_view_mut().copy(self.param.flatten());
-    self.iter_nr = 0;
+    let new_txn = txn();
+
+    // The function is assumed to be already initialized.
+    obj.persist(new_txn, &mut consts_params);
+    let dim = obj.val_size(new_txn, &mut params);
+    assert!(dim > 0);
+
+    let mut param = Vec::with_capacity(dim);
+    param.resize(dim, 0.0);
+    let mut grad = Vec::with_capacity(dim);
+    grad.resize(dim, 0.0);
+    let mut prev_param = Array1d::zeros(dim);
+    let step = Array1d::zeros(dim);
+
+    assert_eq!(dim, obj.store_val(new_txn, &mut params, 0, &mut param));
+    prev_param.as_view_mut().copy(param.flatten());
+
+    Sgd{
+      cfg:          cfg,
+      consts_params:        consts_params,
+      consts_params_grads:  consts_params_grads,
+      params:       params,
+      grads:        grads,
+      obj:          obj,
+      iter_nr:      0,
+      param:        param,
+      grad:         grad,
+      prev_param:   prev_param,
+      step:         step,
+    }
   }
 
   pub fn step<BatchFn>(&mut self, batch_fn: BatchFn) where BatchFn: Fn(TxnId, usize, Rc<AutodiffSink>) {
@@ -66,8 +107,8 @@ impl Sgd {
     for batch_nr in 0 .. num_batches {
       let batch_txn = txn();
       match batch_nr {
-        0 => self.obj.rollover(batch_txn, &mut self.consts_params),
-        _ => self.obj.rollover(batch_txn, &mut self.consts_params_grads),
+        0 => self.obj.persist(batch_txn, &mut self.consts_params),
+        _ => self.obj.persist(batch_txn, &mut self.consts_params_grads),
       }
       let batch_size = min(self.cfg.batch_sz, self.cfg.minibatch_sz - batch_nr * self.cfg.batch_sz);
       batch_fn(batch_txn, batch_size, self.obj.clone());
@@ -76,6 +117,7 @@ impl Sgd {
 
     // Store the gradient.
     let store_txn = txn();
+    self.obj.persist(store_txn, &mut self.grads);
     self.obj.store_grad(store_txn, &mut self.grads, 0, &mut self.grad);
 
     // Calculate the update.
