e46113df5be26bec171143b6f0e7e614fcaaa006
Peter Jin <peterhaijin@gmail.com>
2017-05-01 16:07:14 -0700

    Implement a version of Adam using GPU memory. Currently there are some GPU kernels missing for Adam to work correctly.

--- src/adam.rs
+++ src/adam.rs
@@ -3,6 +3,7 @@ use sgd::*;
 
 use arraydiff::prelude::*;
 use densearray::prelude::*;
+use devicemem_cuda::prelude::*;
 
 use std::cmp::{min};
 use std::rc::{Rc};
@@ -107,6 +108,107 @@ impl Adam {
     self.direction.as_view_mut().elem_ldiv(self.grad_m1.as_view());
     self.direction.as_view_mut().scale(1.0 / (1.0 - (1.0 - self.cfg.gamma1 as f32).powi(self.iter_nr as i32 + 1)));
     self.param.flatten_mut().add(-step_size, self.direction.as_view());
+
+    // Apply the update.
+    let load_txn = txn();
+    self.obj.load_val(load_txn, &mut self.params, 0, &mut self.param);
+
+    // TODO: Optionally, update the batch norm running statistics.
+
+    self.iter_nr += 1;
+  }
+}
+
+pub struct GPUAdam {
+  cfg:          AdamConfig,
+  params:       VarSet,
+  grads:        VarSet,
+  obj:          Rc<AutodiffSink>,
+  iter_nr:      usize,
+  stream:       Rc<DeviceStream>,
+  param:        DeviceMem<f32>,
+  grad:         DeviceMem<f32>,
+  grad_m1:      DeviceArray1d<f32>,
+  grad_m2:      DeviceArray1d<f32>,
+  tmp_buf:      DeviceArray1d<f32>,
+  direction:    DeviceArray1d<f32>,
+}
+
+impl GPUAdam {
+  pub fn new(cfg: AdamConfig, obj: Rc<AutodiffSink>, init_txn: TxnId, param_vars: &VarSet, stream: Rc<DeviceStream>) -> Self {
+    let mut params = param_vars.filter(|v| v.kind == Val);
+    let grads = param_vars.filter(|v| v.kind == Grad);
+
+    // The function is assumed to be already initialized.
+    let dim = obj.val_size(init_txn, &mut params);
+    assert!(dim > 0);
+
+    let param = DeviceMem::zeros(dim, stream.conn());
+    let grad = DeviceMem::zeros(dim, stream.conn());
+    let grad_m1 = DeviceArray1d::zeros(dim, stream.conn());
+    let grad_m2 = DeviceArray1d::zeros(dim, stream.conn());
+    let tmp_buf = DeviceArray1d::zeros(dim, stream.conn());
+    let direction = DeviceArray1d::zeros(dim, stream.conn());
+
+    //assert_eq!(dim, obj.store_val(init_txn, &mut params, 0, &mut param));
+
+    GPUAdam{
+      cfg:          cfg,
+      params:       params,
+      grads:        grads,
+      obj:          obj,
+      iter_nr:      0,
+      stream:       stream,
+      param:        param,
+      grad:         grad,
+      grad_m1:      grad_m1,
+      grad_m2:      grad_m2,
+      tmp_buf:      tmp_buf,
+      direction:    direction,
+    }
+  }
+
+  pub fn iteration_count(&self) -> usize {
+    self.iter_nr
+  }
+
+  pub fn step<BatchFn>(&mut self, mut batch_fn: BatchFn) where BatchFn: FnMut(TxnId, usize, usize, Rc<AutodiffSink>) {
+    // Calculate the gradient.
+    let num_batches = (self.cfg.minibatch_sz + self.cfg.batch_sz - 1) / self.cfg.batch_sz;
+    let mut batch_offset = 0;
+    for batch_nr in 0 .. num_batches {
+      let batch_txn = txn();
+      let batch_size = min(self.cfg.batch_sz, self.cfg.minibatch_sz - batch_nr * self.cfg.batch_sz);
+      batch_fn(batch_txn, batch_offset, batch_size, self.obj.clone());
+      if batch_nr == 0 {
+        self.obj.store_val(batch_txn, &mut self.params, 0, &mut self.param);
+      }
+      if batch_nr == num_batches - 1 {
+        self.obj.store_grad(batch_txn, &mut self.grads, 0, &mut self.grad);
+      }
+      batch_offset += batch_size;
+    }
+
+    // Normalize the gradient by sample size.
+    self.grad.as_mut().flatten_mut().scale(1.0 / self.cfg.minibatch_sz as f32, self.stream.conn());
+
+    // Update the adaptive moments.
+    self.grad_m1.as_view_mut().average(self.cfg.gamma1 as _, self.grad.as_ref().flatten(), self.stream.conn());
+    self.tmp_buf.as_view_mut().copy(self.grad.as_ref().flatten(), self.stream.conn());
+    self.tmp_buf.as_view_mut().square(self.stream.conn());
+    self.grad_m2.as_view_mut().average(self.cfg.gamma2 as _, self.tmp_buf.as_view(), self.stream.conn());
+
+    // Calculate the update.
+    let step_size = self.cfg.step_size.at_iter(self.iter_nr) as f32;
+    self.direction.as_view_mut().copy(self.grad_m2.as_view(), self.stream.conn());
+    self.direction.as_view_mut().scale(1.0 / (1.0 - (1.0 - self.cfg.gamma2 as f32).powi(self.iter_nr as i32 + 1)), self.stream.conn());
+    self.direction.as_view_mut().add_constant(EPSILON_32, self.stream.conn());
+    // FIXME FIXME FIXME
+    unimplemented!();
+    //self.direction.as_view_mut().sqrt(self.stream.conn());
+    //self.direction.as_view_mut().elem_ldiv(self.grad_m1.as_view(), self.stream.conn());
+    self.direction.as_view_mut().scale(1.0 / (1.0 - (1.0 - self.cfg.gamma1 as f32).powi(self.iter_nr as i32 + 1)), self.stream.conn());
+    self.param.as_mut().flatten_mut().add(-step_size, self.direction.as_view(), self.stream.conn());
 
     // Apply the update.
     let load_txn = txn();
